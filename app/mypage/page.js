1926959e19ad9cef3192a6911e8af8fe3819b58b
nasuda <nasuda@example.com>
2023-07-11 19:21:40 +0900

    first commit

---
+++ app/mypage/page.js
@@ -0,0 +1,13 @@
+import Counter from "../../components/counter"
+//import styles from "../mypage.module.css"
+import './styles.css'
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello, WATASHI</h1>
+      <p className="para">css file</p>
+      <Counter />      
+    </div>
+  )
+}
