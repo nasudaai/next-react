3941824cadc7345c97d81c3b925526d272a2b64c
nasuda <nasuda@example.com>
2023-07-13 07:34:23 +0900

    add Link

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import Link from 'next/link'
 export default function Home() {
   return (
+    
     <main className={styles.main}>
+      <Link href="/mypage">mypage</Link>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
