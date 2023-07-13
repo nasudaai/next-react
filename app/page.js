269f197da3630d7ad42d80c5c556cca2162a1678
nasuda <nasuda@example.com>
2023-07-13 19:59:28 +0900

    add Link

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Link from 'next/link'
+import { Mypege } from '@/links/link'
 export default function Home() {
   return (
     
